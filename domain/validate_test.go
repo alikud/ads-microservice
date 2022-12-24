cdb16c66f58d517d8c62fca8ce2468081de9779f
aliaksei-kudravets <frume28@gmail.com>
2022-12-24 10:31:03 +0300

    add some tests

--- domain/validate_test.go
+++ domain/validate_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSomething(t *testing.T) {
+func TestValidationOfferModel(t *testing.T) {
 
 	OkOffer := Offer{
 		Title:       "Iphone 100 Pro max",
@@ -16,11 +16,32 @@ func TestSomething(t *testing.T) {
 
 	require.NoError(t, OkOffer.Validate())
 
-	NotOkOffer := Offer{
+	//Not more 3 links
+	offerManyPhotoUrl := Offer{
 		Title:       "Iphone 10 pro max",
 		Description: "some description",
 		PhotoUrl:    []string{"description1", "description2", "description3", "description4"},
 		Price:       100,
 	}
-	require.Error(t, NotOkOffer.Validate())
+
+	require.Error(t, offerManyPhotoUrl.Validate())
+
+	offerShorTitle := Offer{
+		Title:       "pc",
+		Description: "2 gb 2 cpu best for study",
+		PhotoUrl:    []string{},
+		Price:       500,
+	}
+
+	require.Error(t, offerShorTitle.Validate())
+
+	offerNoPhotos := Offer{
+		Title:       "macbook",
+		Description: "2gb 2cpu best for study",
+		PhotoUrl:    nil,
+		Price:       1233,
+	}
+
+	require.Error(t, offerNoPhotos.Validate())
+
 }
